e6e706ec563a6a1e605c85c1d8cbd02bb5cab523
cs5th <hj666c3@gmail.com>
2019-10-29 18:07:21 +0900

    Create Project and Install Discord.Net (프로젝트 생성과 Discord.Net 설치)

---
+++ SeoaDays/SeoaDays/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeoaDays
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
