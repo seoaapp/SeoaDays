424a80a609279b737670a24cd4f827f1af69c284
cs5th <hj666c3@gmail.com>
2019-10-30 20:21:09 +0900

    made basic bot (기본적인 봇 만듬)

--- SeoaDays/SeoaDays/Program.cs
+++ SeoaDays/SeoaDays/Program.cs
@@ -1,12 +1,51 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace SeoaDays
 {
-    class MainClass
+    class Program
     {
-        public static void Main(string[] args)
+      DiscordSocketClient client = new DiscordSocketClient();
+      public static void Main(string[] args) => new Program().MainAsync().GetAwaiter().GetResult();
+        public async Task MainAsync()
         {
-            Console.WriteLine("Hello World!");
+           string token = System.IO.File.ReadAllText("token.txt");
+            foreach (char t in token)
+            {
+                token = token.Replace("\n", string.Empty);
+            }
+            await client.LoginAsync(TokenType.Bot, token); 
+            await client.StartAsync();
+            client.MessageReceived += Client_MessageReceived;
+            client.Log += Client_Log;
+            client.Ready += Client_Ready;
+            client.GuildAvailable += Client_GuildAvailable;
+            await Task.Delay(-1);
         }
+
+        async Task Client_MessageReceived(SocketMessage message)
+        {
+            Console.WriteLine(message);
+        }
+
+        Task Client_Log(LogMessage arg)
+        {
+            Console.WriteLine(arg.Message);
+            return Task.CompletedTask;
+        }
+
+        Task Client_Ready()
+        {
+            return Task.CompletedTask;
+        }
+
+        Task Client_GuildAvailable(SocketGuild arg)
+        {
+            arg.DefaultChannel.SendMessageAsync("");
+            return Task.CompletedTask;
+        }
+
     }
 }
